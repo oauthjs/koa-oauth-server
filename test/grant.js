39b5879d35715711337c4799ebd885365ca74bf5
Rui Marinho <rpm@seegno.com>
2014-12-18 11:58:03 +0000

    Update copyright notices

--- test/grant.js
+++ test/grant.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-present NightWorld.
+ * Copyright 2014-present Thom Seddon.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
